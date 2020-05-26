e9c775bc0ec58f2e34526e2c8f5134eaffdd1945
Jan Ivar Goli <jan_ivar99@hotmail.com>
2020-05-26 19:41:07 +0200

    File resource should not refer file entry directly. #49

--- source/engine/resources/files/IonFileResource.h
+++ source/engine/resources/files/IonFileResource.h
@@ -13,25 +13,39 @@ File:	IonFileResource.h
 #ifndef ION_FILE_RESOURCE_H
 #define ION_FILE_RESOURCE_H
 
+#include <optional>
+#include <string>
+
 #include "resources/IonResource.h"
-#include "repositories/IonFileRepository.h"
 
 namespace ion::resources::files
 {
 	template <typename T>
 	class FileResource : public Resource<T>
 	{
-		protected:
+		private:
 
-			repositories::file_repository::FileEntry &file_;
+			std::string name_;
+			std::optional<std::string> file_data_;
 
 		public:
 
 			//Constructor
-			FileResource(repositories::file_repository::FileEntry &file) :
-				file_{file}
+			FileResource(std::string name) :
+				name_{std::move(name)}
 			{
 				//Empty
+			}
+
+
+			/*
+				Modifiers
+			*/
+
+			//Sets the file data of the file resource to the given data
+			inline void FileData(std::optional<std::string> data)
+			{
+				file_data_ = std::move(data);
 			}
 
 
@@ -39,17 +53,18 @@ namespace ion::resources::files
 				Observers
 			*/
 
-			//Returns a mutable file entry for this resource
-			[[nodiscard]] inline auto& File() noexcept
+			//Returns the name of the file resource (could be a file name or an entire path)
+			[[nodiscard]] inline auto& Name() const noexcept
 			{
-				return file_;
+				return name_;
 			}
 
-			//Returns an immutable file entry for this resource
-			[[nodiscard]] inline auto& File() const noexcept
+			//Returns the file data of the file resource
+			//Returns nullopt if the file resource has not been prepared, or that it is fully loaded
+			[[nodiscard]] inline auto& FileData() const noexcept
 			{
-				return file_;
-			}
+				return file_data_;
+			}	
 	};
 } //ion::resources::files
 
