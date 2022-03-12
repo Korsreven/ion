41d528490bedf91d92cf594a3d03e3fa0a1681c4
Jan Ivar Goli <jan_ivar99@hotmail.com>
2022-03-12 15:35:37 +0100

    Fixed problems with glTexEnv and glPointParameter for some graphic drivers, when called after glEnable(GL_POINT_SPRITE).

--- source/engine/graphics/scene/IonDrawableParticleSystem.cpp
+++ source/engine/graphics/scene/IonDrawableParticleSystem.cpp
@@ -74,17 +74,17 @@ void enable_point_sprites() noexcept
 	switch (gl::PointSprite_Support())
 	{
 		case gl::Extension::Core:
-		glEnable(GL_POINT_SPRITE); //Enable point sprite
+		glTexEnvi(GL_POINT_SPRITE, GL_COORD_REPLACE, GL_TRUE); //Enable sprite tex coords
+		glPointParameteri(GL_POINT_SPRITE_COORD_ORIGIN, GL_LOWER_LEFT); //Set 0,0 to lower left
 		glEnable(GL_VERTEX_PROGRAM_POINT_SIZE); //Enable varying point size
-		glTexEnvf(GL_POINT_SPRITE, GL_COORD_REPLACE, GL_TRUE); //Enable sprite tex coords
-		glPointParameterf(GL_POINT_SPRITE_COORD_ORIGIN, GL_LOWER_LEFT); //Set 0,0 to lower left
+		glEnable(GL_POINT_SPRITE); //Enable point sprite
 		break;
 
 		case gl::Extension::ARB:
-		glEnable(GL_POINT_SPRITE_ARB); //Enable point sprite
+		glTexEnvi(GL_POINT_SPRITE_ARB, GL_COORD_REPLACE_ARB, GL_TRUE); //Enable sprite tex coords
+		glPointParameteri(GL_POINT_SPRITE_COORD_ORIGIN, GL_LOWER_LEFT); //Set 0,0 to lower left
 		glEnable(GL_VERTEX_PROGRAM_POINT_SIZE_ARB); //Enable varying point size
-		glTexEnvf(GL_POINT_SPRITE_ARB, GL_COORD_REPLACE_ARB, GL_TRUE); //Enable sprite tex coords
-		glPointParameterf(GL_POINT_SPRITE_COORD_ORIGIN, GL_LOWER_LEFT); //Set 0,0 to lower left
+		glEnable(GL_POINT_SPRITE_ARB); //Enable point sprite
 		break;
 	}
 }
@@ -94,15 +94,15 @@ void disable_point_sprites() noexcept
 	switch (gl::PointSprite_Support())
 	{
 		case gl::Extension::Core:
-		glPointParameterf(GL_POINT_SPRITE_COORD_ORIGIN, GL_UPPER_LEFT); //Set 0,0 back to upper left
-		glTexEnvf(GL_POINT_SPRITE, GL_COORD_REPLACE, GL_FALSE); //Disable sprite tex coords
+		glPointParameteri(GL_POINT_SPRITE_COORD_ORIGIN, GL_UPPER_LEFT); //Set 0,0 back to upper left
+		glTexEnvi(GL_POINT_SPRITE, GL_COORD_REPLACE, GL_FALSE); //Disable sprite tex coords
 		glDisable(GL_VERTEX_PROGRAM_POINT_SIZE); //Disable varying point size
 		glDisable(GL_POINT_SPRITE); //Disable point sprite
 		break;
 
 		case gl::Extension::ARB:
-		glPointParameterf(GL_POINT_SPRITE_COORD_ORIGIN, GL_UPPER_LEFT); //Set 0,0 back to upper left
-		glTexEnvf(GL_POINT_SPRITE_ARB, GL_COORD_REPLACE_ARB, GL_FALSE); //Disable sprite tex coords
+		glPointParameteri(GL_POINT_SPRITE_COORD_ORIGIN, GL_UPPER_LEFT); //Set 0,0 back to upper left
+		glTexEnvi(GL_POINT_SPRITE_ARB, GL_COORD_REPLACE_ARB, GL_FALSE); //Disable sprite tex coords
 		glDisable(GL_VERTEX_PROGRAM_POINT_SIZE_ARB); //Disable varying point size
 		glDisable(GL_POINT_SPRITE_ARB); //Disable point sprite
 		break;
