ebc292f722014fc8a75f7f7eba5ae73f4ff15cf7
Jan Ivar Goli <jan_ivar99@hotmail.com>
2022-04-10 10:08:09 +0200

    Missing frame sequence script interface include in main. #156

--- source/main.cpp
+++ source/main.cpp
@@ -185,6 +185,7 @@ File:	main.cpp
 #include "script/IonScriptValidator.h"
 #include "script/interfaces/IonAnimationScriptInterface.h"
 #include "script/interfaces/IonFontScriptInterface.h"
+#include "script/interfaces/IonFrameSequenceScriptInterface.h"
 #include "script/interfaces/IonMaterialScriptInterface.h"
 #include "script/interfaces/IonParticleSystemScriptInterface.h"
 #include "script/interfaces/IonScriptInterface.h"
