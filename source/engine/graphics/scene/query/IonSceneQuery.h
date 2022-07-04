76779f0e1231e7db5d41a5f5e9991583bd74c66c
Jan Ivar Goli <jan_ivar99@hotmail.com>
2022-07-04 17:35:04 +0200

    Removed Animation from QueryType. #163

--- source/engine/graphics/scene/query/IonSceneQuery.h
+++ source/engine/graphics/scene/query/IonSceneQuery.h
@@ -40,16 +40,15 @@ namespace ion::graphics::scene::query
 			static constexpr uint32 Movable = 1 << 0;
 			static constexpr uint32 Drawable = 1 << 1;
 
-			static constexpr uint32 Animation = 1 << 2;
-			static constexpr uint32 Camera = 1 << 3;
-			static constexpr uint32 Light = 1 << 4;
-			static constexpr uint32 Model = 1 << 5;
-			static constexpr uint32 ParticleSystem = 1 << 6;
-			static constexpr uint32 Sound = 1 << 7;
-			static constexpr uint32 SoundListener = 1 << 8;
-			static constexpr uint32 Text = 1 << 9;
+			static constexpr uint32 Camera = 1 << 2;
+			static constexpr uint32 Light = 1 << 3;
+			static constexpr uint32 Model = 1 << 4;
+			static constexpr uint32 ParticleSystem = 1 << 5;
+			static constexpr uint32 Sound = 1 << 6;
+			static constexpr uint32 SoundListener = 1 << 7;
+			static constexpr uint32 Text = 1 << 8;
 
-			static constexpr uint32 UserType0 = 1 << 10;
+			static constexpr uint32 UserType0 = 1 << 9;
 				//First (pot) value not in use by the engine
 		};
 
